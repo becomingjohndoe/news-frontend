0ae64f596a8e086e4066e80df400c2d755b8ec84
becomingjohndoe <karl.swatman@gmail.com>
2021-11-23 12:53:30 +0000

    added link to topic in article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import CommentsButton from "./CommentsButton";
 import LikeButton from "./LikeButton";
-export default function ArticleCard({ article, bodyClass }) {
+
+export default function ArticleCard({ article, bodyClass, linkClass }) {
 	return (
 		<div className="article-card">
 			<h3>{article.title}</h3>
 			<p className={bodyClass}>{article.body}</p>
 			<span>See more</span>
 			<p>
-				in {article.topic} by {article.author}
+				in{" "}
+				<Link className={linkClass} to={`articles/topic/${article.topic}`}>
+					{article.topic}
+				</Link>{" "}
+				by {article.author}
 			</p>
 			<LikeButton votes={article.votes} />
 			<CommentsButton count={article.comment_count} id={article.article_id} />
