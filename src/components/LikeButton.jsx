4367f4961a1b56d40673ee429570cb165d9ba948
becomingjohndoe <karl.swatman@gmail.com>
2021-11-24 11:01:09 +0000

    adds upvote downvote functionality for  articles

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,9 +1,26 @@
-export default function LikeButton({ votes }) {
+import { useState } from "react";
+import { patchArticleVotes } from "../utils/api";
+
+export default function LikeButton({ votes = 0, id }) {
+	const [likes, setLikes] = useState(0);
+
+	const handleClick = (e) => {
+		let vote = 0;
+		if (e.target.className === "fas fa-arrow-up") {
+			vote = 1;
+		}
+		if (e.target.className === "fas fa-arrow-down") {
+			vote = -1;
+		}
+		patchArticleVotes(id, vote).then(() => {});
+		setLikes(likes + vote);
+	};
+
 	return (
 		<div className="like-button">
-			<i className="fas fa-arrow-up"></i>
-			<span className="votes">{votes}</span>
-			<i className="fas fa-arrow-down"></i>
+			<i name="up" onClick={handleClick} className="fas fa-arrow-up"></i>
+			<span className="votes">{votes + likes}</span>
+			<i name="down" onClick={handleClick} className="fas fa-arrow-down"></i>
 		</div>
 	);
 }
