50cdbbadd1301a1443fde04b7194ecce33566363
becomingjohndoe <karl.swatman@gmail.com>
2021-11-24 10:59:04 +0000

    adds browser router to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { UserProvider } from "./components/context/userContext";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<UserProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</UserProvider>,
 	document.getElementById("root")
 );
