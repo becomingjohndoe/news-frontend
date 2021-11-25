b4e18a143811e3fda8adb7ec051199419bdc3652
becomingjohndoe <karl.swatman@gmail.com>
2021-11-25 14:40:04 +0000

    adds loading state to single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -3,15 +3,25 @@ import { useParams } from "react-router";
 import { getArticleById } from "../utils/api";
 import ArticleCard from "./ArticleCard";
 import ArticleComments from "./ArticleComments";
-
+import loadingIcon from "../images/toppit(3).gif";
 export default function SingleArticle() {
 	const [article, setArticle] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 	const { article_id } = useParams();
 	useEffect(() => {
-		getArticleById(article_id).then((article) => {
-			setArticle(article);
-		});
+		setIsLoading(true);
+		setTimeout(() => {
+			getArticleById(article_id).then(setArticle);
+			setIsLoading(false);
+		}, 1000);
 	}, [article_id]);
+	if (isLoading || article.length === 0) {
+		return (
+			<div className="loading-icon-container">
+				<img className="loading-icon" src={loadingIcon} alt="is-loading" />
+			</div>
+		);
+	}
 	return (
 		<>
 			<ArticleCard article={article} bodyClass="article-body-full" />
