13eaf6357b7843ab802243cc181c776cb2a838ec
becomingjohndoe <karl.swatman@gmail.com>
2021-11-25 14:38:48 +0000

    adds logo and user info to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
-
+import UserAvatar from "./UserInfo";
 export default function Nav() {
 	return (
 		<nav className="navbar">
-			<Link to="/">LOGO</Link>
-			<Link to="/">USER</Link>
+			<Link to="/">
+				{/* <img className="logo" src={logo} alt="logo" /> */}
+				<span className="logo">topp</span>
+				<span className="logo-i">i</span>
+				<span className="logo">t</span>
+			</Link>
+			<Link to="/">
+				<UserAvatar username="jessjelly" />
+			</Link>
 		</nav>
 	);
 }
