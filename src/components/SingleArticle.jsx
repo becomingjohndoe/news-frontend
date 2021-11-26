8b0e2c4a853ed5f8d2cf6a764d6cd1f8cff6078d
becomingjohndoe <karl.swatman@gmail.com>
2021-11-26 10:41:23 +0000

    error handling for single article page

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,17 +4,29 @@ import { getArticleById } from "../utils/api";
 import ArticleCard from "./ArticleCard";
 import ArticleComments from "./ArticleComments";
 import loadingIcon from "../images/toppit(3).gif";
+import NotFound from "./NotFound";
 export default function SingleArticle() {
 	const [article, setArticle] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [err, setErr] = useState(null);
 	const { article_id } = useParams();
 	useEffect(() => {
 		setIsLoading(true);
 		setTimeout(() => {
-			getArticleById(article_id).then(setArticle);
-			setIsLoading(false);
-		}, 1000);
+			getArticleById(article_id)
+				.then((data) => {
+					setArticle(data);
+					setIsLoading(false);
+				})
+				.catch((err) => {
+					setIsLoading(false);
+					setErr(err);
+				});
+		}, 700);
 	}, [article_id]);
+	if (err) {
+		return <NotFound />;
+	}
 	if (isLoading || article.length === 0) {
 		return (
 			<div className="loading-icon-container">
