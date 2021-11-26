4a1904dae75f970356d34f291b9ffd2e5e034131
becomingjohndoe <karl.swatman@gmail.com>
2021-11-26 10:42:22 +0000

    adds loading state and error handling to topic page

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -3,15 +3,35 @@ import { useParams } from "react-router";
 import { getArticlesByTopic } from "../utils/api";
 import ArticleCard from "./ArticleCard";
 import Controls from "./Controls";
-
+import NotFound from "./NotFound";
+import loadingIcon from "../images/toppit(3).gif";
 export default function Topic() {
 	const { topic } = useParams();
 	const [articles, setArticles] = useState([]);
+	const [err, setErr] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
-		getArticlesByTopic(topic).then((article) => {
-			setArticles(article);
-		});
+		setIsLoading(true);
+		getArticlesByTopic(topic)
+			.then((article) => {
+				setIsLoading(false);
+				setArticles(article);
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				setErr(err);
+			});
 	}, [topic]);
+	if (err) {
+		return <NotFound />;
+	}
+	if (isLoading || articles.length === 0) {
+		return (
+			<div className="loading-icon-container">
+				<img className="loading-icon" src={loadingIcon} alt="is-loading" />
+			</div>
+		);
+	}
 	return (
 		<div className="articles">
 			<h1 className="App-header">{topic}</h1>
