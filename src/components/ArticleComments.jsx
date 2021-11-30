ecca4830be1989772a8f929aa8498721d4e94970
becomingjohndoe <karl.swatman@gmail.com>
2021-11-30 12:34:12 +0000

    adds edit button to comment

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { deleteComment, getArticleComments } from "../utils/api";
+import { getArticleComments } from "../utils/api";
 import { timeElapsed } from "../utils/timeElapsed";
 import { userContext } from "./context/userContext";
 import DeleteComment from "./DeleteComment";
@@ -12,29 +12,40 @@ export default function ArticleComments({ article_id }) {
 	const [comments, setComments] = useState([]);
 	const [posted, setPosted] = useState(false);
 	const [deleted, setDeleted] = useState(false);
+	const [edit, setEdit] = useState(false);
+	const [idOfEdit, setIdOfEdit] = useState(null);
 	useEffect(() => {
 		getArticleComments(article_id).then(setComments);
 	}, [article_id, posted, deleted]);
 	return (
-		<div>
+		<div className="comments">
 			<PostComment setPosted={setPosted} id={article_id} />
 
 			{comments.map((comment) => (
 				<div className="comment-container" key={`${comment.comment_id}`}>
 					<UserInfo username={comment.author} />
-					<p className="comment-body">{comment.body}</p>
-					<LikeButton
-						votes={comment.votes}
-						id={comment.comment_id}
-						isComment={true}
-					/>
-					<i className="far fa-clock"> {timeElapsed(comment.created_at)}</i>
-					<i className=""></i>
-					<DeleteComment
-						id={comment.comment_id}
-						isVisible={user === comment.author}
-						setDeleted={setDeleted}
-					/>
+
+					<div className="comment-body">
+						{edit && idOfEdit === comment.comment_id ? <PostComment /> : comment.body}
+					</div>
+
+					<div className="comment-info">
+						<LikeButton
+							votes={comment.votes}
+							id={comment.comment_id}
+							isComment={true}
+						/>
+						<div className="time-elapsed">
+							<i className="far fa-clock"> {timeElapsed(comment.created_at)}</i>
+						</div>
+						<DeleteComment
+							id={comment.comment_id}
+							isVisible={user === comment.author}
+							setDeleted={setDeleted}
+							setEdit={setEdit}
+							setIdOfEdit={setIdOfEdit}
+						/>
+					</div>
 				</div>
 			))}
 		</div>
