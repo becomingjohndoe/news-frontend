5972493655d890fec080988391874e3a0d87a29e
becomingjohndoe <karl.swatman@gmail.com>
2021-11-30 12:36:34 +0000

    error handling for posting a comment

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,27 +1,64 @@
 import { userContext } from "./context/userContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { postComment } from "../utils/api";
+import UserAvatar from "./UserInfo";
 
 export default function PostComment({ id, setPosted }) {
+	const [err, setErr] = useState(null);
 	const { user } = useContext(userContext);
 	// post comment
 	const handleSubmit = (event) => {
-		setPosted(true);
 		event.preventDefault();
+		setPosted(true);
 		const newComment = {
 			username: user,
 			body: event.target.comment.value,
 		};
-		postComment(id, newComment).then(() => {
-			setPosted(false);
-		});
+		postComment(id, newComment)
+			.then(() => {
+				setPosted(false);
+			})
+			.catch((err) => {
+				setErr(err);
+			});
 	};
+	if (err) {
+		return (
+			<div>
+				<p>Something went wrong</p>
+				<button
+					className="btn-danger"
+					onClick={() => {
+						setErr(null);
+					}}
+				>
+					Try Again?
+				</button>
+			</div>
+		);
+	}
 	return (
-		<form className="post-comment-container" onSubmit={handleSubmit}>
-			<button className="post-comment-button" type="submit">
-				Post
-			</button>
-			<input type="text" name="comment" id="comment" />
-		</form>
+		<>
+			<form className="post-comment-container" onSubmit={handleSubmit}>
+				<div className="posting-as">
+					<p>Posting as: </p>
+					<UserAvatar username={user} />
+				</div>
+				<textarea
+					type="text"
+					name="comment"
+					id="comment"
+					defaultValue="What's on your mind?"
+					onFocus={(e) => {
+						if (e.target.value === e.target.defaultValue) {
+							e.target.value = "";
+						}
+					}}
+				/>
+				<button className="post-comment-button btn" type="submit">
+					Post
+				</button>
+			</form>
+		</>
 	);
 }
