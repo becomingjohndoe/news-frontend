c1fede48d3d323e245c62ccda465e4b4ee630bc9
becomingjohndoe <karl.swatman@gmail.com>
2021-11-30 12:37:21 +0000

    loading state for user info component

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,13 +3,21 @@ import { useEffect, useState } from "react";
 
 export default function UserAvatar({ username }) {
 	const [user, setUser] = useState([]);
-
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		getUserByUsername(username).then((data) => {
 			setUser(data);
+			setIsLoading(false);
 		});
+		// cleanup
+		return () => {
+			setUser([]);
+			setIsLoading(true);
+		};
 	}, [username]);
-
+	if (isLoading) {
+		return <></>;
+	}
 	return (
 		<div className="user-info">
 			<img className="user-avatar" src={user.avatar_url} alt={user.username} />
