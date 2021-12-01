cbdeaa27a09a86e5e7bf5ffc5fd73c29cc3e88a6
becomingjohndoe <karl.swatman@gmail.com>
2021-12-01 18:01:40 +0000

    adds dropdown navigation to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
 import UserAvatar from "./UserInfo";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 export default function Nav() {
 	return (
-		<nav className="navbar">
-			<Link to="/">
-				{/* <img className="logo" src={logo} alt="logo" /> */}
-				<span className="logo">topp</span>
-				<span className="logo-i">i</span>
-				<span className="logo">t</span>
-			</Link>
+		<nav className="navbar fixed-top">
+			<div className="nav-items">
+				<Link to="/">
+					{/* <img className="logo" src={logo} alt="logo" /> */}
+					<span className="logo">topp</span>
+					<span className="logo-i">i</span>
+					<span className="logo">t</span>
+				</Link>
+				<DropdownButton
+					title={
+						<i class="fas fa-home">
+							{" "}
+							<span>Home</span>
+						</i>
+					}
+					id="dropdown-menu-align-responsive-1"
+					drop="down"
+					align={{ lg: "end" }}
+				>
+					<Dropdown.Item>Hello there!</Dropdown.Item>
+				</DropdownButton>
+			</div>
 			<Link to="/">
 				<UserAvatar username="jessjelly" />
 			</Link>
