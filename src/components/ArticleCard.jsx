6456babe8385d4643b5067815e26c43b6f9839d6
becomingjohndoe <karl.swatman@gmail.com>
2021-12-01 18:02:13 +0000

    generate random memes for testing

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getMemes } from "../utils/api";
 import { timeElapsed } from "../utils/timeElapsed";
 import CommentsButton from "./CommentsButton";
 import LikeButton from "./LikeButton";
 import UserAvatar from "./UserInfo";
 export default function ArticleCard({ article, bodyClass, linkClass }) {
+	const [meme, setMeme] = useState([]);
+	// generate random number between 100 500
+	// generate random number between 200 and 600
+	useEffect(() => {
+		getMemes().then((data) => {
+			console.log(data);
+			setMeme(data);
+		});
+	}, [article]);
+
 	return (
 		<div className="article-card">
 			<div className="article-card-info">
@@ -16,6 +28,9 @@ export default function ArticleCard({ article, bodyClass, linkClass }) {
 			<Link to={`/articles/${article.article_id}`}>
 				<h5>{article.title}</h5>
 			</Link>
+			<div className="article-image-container">
+				<img className="article-image" src={meme.url} alt="article-image" />
+			</div>
 			<p className={bodyClass}>{article.body}</p>
 
 			<LikeButton votes={article.votes} id={article.article_id} />
